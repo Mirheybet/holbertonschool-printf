78745f1a0ce3026e7505c9bab63d29a70b0733ac
Mirheybet <mirheybet.hesenov@gmail.com>
2024-07-24 07:56:41 +0000

    try3-printf(char,string,percent)

--- printfcsp.c
+++ printfcsp.c
@@ -1,38 +1,92 @@
+#include <stdio.h>
 #include <stdarg.h>
 #include "main.h"
 
 /**
- * _printf - It is our printf function.
- * @format: variable of function , first element of variadic function
- * Return: It returns size of args .
+ * print_string - This functions is for print of string
+ * @sizestr: int variable | for size
+ * @str: array of char variable | for input string
+ * Return: size of string
+ */
+
+int print_string(int sizestr, char *str)
+{
+	int index = 0;
+
+	if (!str)
+	{
+		_putchar('(');
+		_putchar('n');
+		_putchar('u');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+		sizestr = sizestr + 6;
+	}
+	else
+	{
+		while (str[index] != '\0')
+		{
+			_putchar(str[index]);
+			index++;
+			sizestr++;
+		}
+	}
+	return (sizestr);
+}
+
+/**
+ * print_char - This function is for printf of char
+ * @sizechar: variable of integer | for size
+ * @varchar: variable of char | for input char
+ * Return: size of char
+ */
+
+int print_char(int sizechar, char varchar)
+{
+	_putchar(varchar);
+	sizechar++;
+	return (sizechar);
+}
+
+/**
+ * _printf - main printf (char , string , percent)
+ * @format: variable of char array | it is main variable
+ * Return: size of main input
  */
 
 int _printf(const char *format, ...)
 {
-	int indexchar = 0, indexstr = 0, sizeofargument = 0;
-	char *varofargs;
-
 	va_list(args);
+	int indexelement = 0, size = 0;
+
+	if (format == NULL)
+		return (-1);
+	if (*format == '%' && *(format + 1) == 0)
+		return (-1);
 
 	va_start(args, format);
-	while (format[indexchar])
+
+	while (format[indexelement])
 	{
-		if (format[indexchar] == '%' && format[indexchar + 1] == '\0')
-			return (-1);
-		else if (format[indexchar] == '%' && format[indexchar + 1] == '%')
-			_putchar('%'), sizeofargument++, indexchar += 2;
-		else if (format[indexchar] == '%' && format[indexchar + 1] == 'c')
-			_putchar(va_arg(args, int)), sizeofargument++, indexchar = indexchar + 2;
-		else if (format[indexchar] == '%' && format[indexchar + 1] == 's')
+		if (format[indexelement] == '%' && format[indexelement + 1] == 'c')
 		{
-			varofargs = va_arg(args, char*);
-			while (varofargs[indexstr])
-				_putchar(varofargs[indexstr]), varofargs++;
-			sizeofargument += indexstr, indexchar += 2;
+			size = print_char(size, (char)va_arg(args, int)), indexelement += 2;
+		}
+		else if (format[indexelement] == '%' && format[indexelement + 1] == 's')
+		{
+			size = print_string(size, va_arg(args, char *)), indexelement += 2;
+		}
+		else if (format[indexelement] == '%' && format[indexelement + 1] == '%')
+		{
+			_putchar('%'), size++, indexelement += 2;
 		}
 		else
-			_putchar(format[indexchar]), indexchar++, sizeofargument++;
+		{
+			size++, _putchar(format[indexelement]), indexelement++;
+		}
+
 	}
 	va_end(args);
-	return (sizeofargument);
+	return (size);
 }
