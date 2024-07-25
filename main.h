14c7a23a62716aa65795b158b77388405fc213d3
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-07-25 09:38:45 +0000

    from main.h

--- main.h
+++ main.h
@@ -1,10 +1,10 @@
-#ifndef MAIN_h
-#define MAIN_h
-
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
 int _printf(const char *format, ...);
-int printf_string(int strsize, char *str);
-int printf_char(int charsize, char charvar);
-int print_decimal(int value);
-
+int _putchar(char c);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_int(va_list arg);
 #endif
